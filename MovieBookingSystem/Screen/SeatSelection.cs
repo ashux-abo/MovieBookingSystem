8635df994f0e6291261018ac03db0b8dcc59fbc0
ashleyhi0-0 <dulayashley12@gmail.com>
2025-05-12 13:20:28 +0800

    making of path file

---
+++ MovieBookingSystem/Screen/SeatSelection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MovieBookingSystem.Screen
+{
+    public partial class SeatSelection : UserControl
+    {
+        public SeatSelection()
+        {
+            InitializeComponent();
+            SeatSelection_Load(this, EventArgs.Empty); // Call the load method to initialize the seats
+        }
+        private int row = 5;
+        private int column = 10;
+
+        private void SeatSelection_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    var seatButton = new Guna.UI2.WinForms.Guna2Button();
+                    seatButton.Size = new Size(50, 50);
+                    seatButton.Location = new Point(j * 60, i * 60);
+                    seatButton.Text = $"{(char)('A' + i)}{j + 1}"; // Seat label (e.g., A1, A2, ...)
+                  
+                   
+                }
+            }
+        }
+    }
+}
