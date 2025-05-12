44408f41f1aa878ea91be0e0aa89c19d9b5a340e
Jane <mariijane818@gmail.com>
2025-05-12 13:00:15 +0800

    Add files via upload

---
+++ PaymentMethod/PaymentControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaymentMethod
+{
+    public partial class PaymentControl : UserControl
+    {
+        public PaymentControl()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2HtmlLabel5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
